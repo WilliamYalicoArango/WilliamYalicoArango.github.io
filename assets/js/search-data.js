085f42329f20263492b724259ffdfc0aba6e281d
WilliamYalicoArango <familionyalicos@gmail.com>
2026-05-17 05:15:05 +0000

    Deploying to gh-pages from @ WilliamYalicoArango/WilliamYalicoArango.github.io@d391fcaa04483714f5763bdf4e75ec0cc30bd8a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -102,13 +102,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/william-eduardo-yalico-arango", "_blank");
         },
       },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/WilliamYalicoArango", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
