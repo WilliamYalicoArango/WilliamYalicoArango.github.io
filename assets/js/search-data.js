9567ae5c815a2409093aa9d37bac05daf0985171
WilliamYalicoArango <familionyalicos@gmail.com>
2026-05-17 02:18:49 +0000

    Deploying to gh-pages from @ WilliamYalicoArango/WilliamYalicoArango.github.io@29b6b5948786c91532295f57d4b353d3122f4cf4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -113,7 +113,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%77%69%6C%6C%69%61%6D.%79%61%6C%69%63%6F@%75%74%65%63.%65%64%75.%70%65", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -134,14 +134,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=", "_blank");
         },
       },{
       id: 'light-theme',
