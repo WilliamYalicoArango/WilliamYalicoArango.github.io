4619e98b42fcd5282af44fe1348b22edbae65ce7
WilliamYalicoArango <familionyalicos@gmail.com>
2026-05-17 01:47:50 +0000

    Deploying to gh-pages from @ WilliamYalicoArango/WilliamYalicoArango.github.io@dddaf982f6ea073e300cf23cf457b7b6aa852dec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-william-e-yalico-arango",
+    title: "William E. Yalico Arango",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
